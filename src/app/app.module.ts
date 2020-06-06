9a5837493cdee1b6ac8d9b6ca08b033181c36e8e
Максим <kolkov.2000@mail.ru>
2020-06-06 20:18:36 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TableWorkersComponent } from './ui/table-workers/table-workers.component';
+import { AddformWorkerComponent } from './ui/addform-worker/addform-worker.component';
+import { from } from 'rxjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent, TableWorkersComponent, AddformWorkerComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
